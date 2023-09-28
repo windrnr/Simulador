d23c459980b68df8deb7a5aef7c86beebda657a4
Lau <acostaquintanalautaro@gmail.com>
2023-09-28 17:03:00 -0300

    :P

--- sim_mod/process.py
+++ sim_mod/process.py
@@ -5,13 +5,6 @@ class Process:
         self.TA     = data[2]
         self.TI     = data[3]
 
-    # Esto está acá únicamente para que vean que funciona la carga de los atributos jsjs
-    def print_own_data(self):
-        print(f"PDI:    {self.PID}")
-        print(f"SIZE:   {self.SIZE}")
-        print(f"TA:     {self.TA}")
-        print(f"TI:     {self.TI}")
-
     def return_list_of_data(self) -> list:
         data = []
         data.append(self.PID)
