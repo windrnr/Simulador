f2e27e65e71dc957eea91ff641068a25975be15c
Lau <acostaquintanalautaro@gmail.com>
2023-09-28 01:45:08 -0300

    añadí algunos cambios y un prototipo para generar un dictionary

--- src/test.py
+++ src/test.py
@@ -3,7 +3,8 @@ from pathlib import Path
 import csv
 from tabulate import tabulate
 
-def readCSV(file_path: Path) -> list[str]:
+
+def read_csv(file_path: Path) -> list[str]:
     data = []
     if not Path.exists(file_path):
         raise ValueError(f"A ocurrido un error abriendo '{file_path}'.")
@@ -16,27 +17,42 @@ def readCSV(file_path: Path) -> list[str]:
     return data
 
 
+# Revisar esto, no funciona correctamente porque todavía no conozco cómo funcionan los dictionarys, más precisamente como añadir nuevas entradas a uno.
+def read_csv_dictionary(file_path: Path) -> dict[str, int]:
+    data = {}
+    
+    if not Path.exists(file_path):
+        raise ValueError(f"A ocurrido un error abriendo '{file_path}'.")
+    with open(file_path, mode='r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            data.update(row)
+
+    return data
+
+
+def print_table(data:list[str]) -> None:
+    headers = ["PDI", "TAM(KB)", "TA", "TI"]
+    print(tabulate(data, headers, tablefmt="grid"))
+
+
 def main() -> None:
     parser = argparse.ArgumentParser()
-    parser.add_argument("file_path", type=Path, help="Dirección del archivo con los procesos.")
+    parser.add_argument(
+        "file_path", type=Path, help="Dirección del archivo con los procesos."
+    )
     args = parser.parse_args()
 
-    if not (args.file_path.suffix == '.csv'):
+    if not (args.file_path.suffix == ".csv"):
         parser.error("El archivo debe ser de extensión '.csv'.")
-    
-    headers = ["PDI", "TAM(KB)", "TA", "TI"]
+
     try:
-        data = readCSV(args.file_path)
+        data = read_csv(args.file_path)
+        print_table(data)
     except ValueError as e:
         print(f"Error: {e}")
         parser.error("El archivo no existe o no es encontrado.")
 
 
-    print(tabulate(data, headers, tablefmt="grid"))
-
-
 if __name__ == "__main__":
     main()
-
-
-
